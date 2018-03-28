5146721d9979f6693ddf9e035e2ab37ed7603910
Kishor Sonaiya <33340303+kishorsonaiya@users.noreply.github.com>
2018-03-28 11:03:16 +0530

    ChromeDriver Changes

--- src/test/java/stepDefinition/TestSteps.java
+++ src/test/java/stepDefinition/TestSteps.java
@@ -26,7 +26,7 @@ public class TestSteps {
 	public void user_is_on_Home_Page() throws Throwable {
 		System.setProperty("webdriver.ie.driver", "//opt//selenium//chromedriver");
 
-		DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
+		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
 		// this line of code is to resolve protected mode issue
 		// capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS,
 		// true);
