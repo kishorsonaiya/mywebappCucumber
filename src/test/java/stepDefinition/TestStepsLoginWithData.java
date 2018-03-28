09dd2543fc14526dc8fd698c5c00ae4f0d9fb7fb
Kishor Sonaiya <33340303+kishorsonaiya@users.noreply.github.com>
2018-03-28 10:54:01 +0530

    chromedriver

--- src/test/java/stepDefinition/TestStepsLoginWithData.java
+++ src/test/java/stepDefinition/TestStepsLoginWithData.java
@@ -22,7 +22,7 @@ public class TestStepsLoginWithData {
 
 	@Given("^User is on Login Page$")
 	public void user_is_on_Home_Page() throws Throwable {
-		System.setProperty("webdriver.ie.driver", "//opt//selenium//IEDriverServer.exe");
+		System.setProperty("webdriver.ie.driver", "//opt//selenium//chromedriver");
 
 		DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
 		// this line of code is to resolve protected mode issue
