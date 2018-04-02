a40ba70bb3d01292ec2ec25c6e2e969ce7b42e70
Kishor Sonaiya <33340303+kishorsonaiya@users.noreply.github.com>
2018-04-02 16:53:04 +0530

    Update TestStepsLoginWithData.java

--- src/test/java/stepDefinition/TestStepsLoginWithData.java
+++ src/test/java/stepDefinition/TestStepsLoginWithData.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -21,31 +22,9 @@ public class TestStepsLoginWithData {
 	@Given("^User is on Login Page$")
 	public void user_is_on_Home_Page() throws Throwable {
 		System.setProperty("webdriver.gecko.driver", "//opt//selenium//geckodriver");
-
-		
-
-		// without this setting, page will get open and then get closed
-		//capabilities.setCapability(ChromeDriver.INITIAL_BROWSER_URL, "");
-
-		// capabilities.setCapability(InternetExplorerDriver.INITIAL_BROWSER_URL,
-		// "http://localhost:8090/mywebapp/userlogin.jsp");
-
-		// this line of code is to resolve protected mode issue
-		//capabilities.setCapability(ChromeDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
-
-		// **VERY important** adding this line make launching the webapp site
-		//capabilities.setCapability(ChromeDriver.IGNORE_ZOOM_SETTING, true);
-
-		// Adding INITIAL_BROWSER_URL, help to load the IE with required url.
-		// Otherwise it will open browser with default url and then redirect to
-		// application url
-		//capabilities.setCapability(ChromeDriver.INITIAL_BROWSER_URL, "http://10.177.162.26:9080/mywebapp/userlogin.jsp");
-
-		// using this feature, sendKeys function was with good speed. Other wise
-		// it takes 3 sends to type one character.
-		//capabilities.setCapability(ChromeDriver.NATIVE_EVENTS, false);
-
-		driver = new FirefoxDriver();
+		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+		capabilities.setCapability("marionette", false);
+		driver = new FirefoxDriver(capabilities);
 		String baseUrl = "http://10.177.162.26:9080/mywebapp/userlogin.jsp";
 		driver.manage().window().maximize();
 		driver.navigate().to(baseUrl);
