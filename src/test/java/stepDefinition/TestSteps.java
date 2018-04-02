688178ae1e8925212c0b79217675f650cb9932b7
Kishor Sonaiya <33340303+kishorsonaiya@users.noreply.github.com>
2018-04-02 15:31:35 +0530

    Update TestSteps.java

--- src/test/java/stepDefinition/TestSteps.java
+++ src/test/java/stepDefinition/TestSteps.java
@@ -3,7 +3,7 @@ package stepDefinition;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
